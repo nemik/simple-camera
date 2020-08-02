81f3fccf4cba71f0bc8a3e2cdfd9a90b92dfa086
Nemanja Stefanović <nemik@nemik.net>
2020-08-02 13:44:46 -0500

    finally got gallery deletion down

--- lib/gallery.dart
+++ lib/gallery.dart
@@ -26,23 +26,24 @@ class _GalleryPageState extends State<GalleryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      /*
-      appBar: AppBar(
-        title: Text('Gallery'),
-      ),
-      */
-      // Implemented with a PageView, simpler than setting it up yourself
-      // You can either specify images directly or by using a builder as in this tutorial
       body: new FutureBuilder<List>(
         future: getFiles(),
         builder: (context, photos) {
           if (photos.hasError) print(photos.error);
 
+          if (current_file != null &&
+              !current_file.existsSync() &&
+              photos.hasData &&
+              photos.data.length > 0) {
+            current_file = photos.data.reversed.toList()[0];
+            print("init not found ${current_file}");
+          }
+
           if (current_file == null &&
               photos.hasData &&
               photos.data.length > 0) {
             current_file = photos.data.reversed.toList()[0];
-            print("init ${current_file}");
+            print("init null ${current_file}");
           }
 
           return photos.hasData && photos.data.length > 0
@@ -84,20 +85,10 @@ class _GalleryPageState extends State<GalleryPage> {
                           iconData: Icons.delete,
                           color: Colors.red,
                           onTap: () {
-                            /*
-                            final snackBar = SnackBar(
-                                content: Text('Yay! ' + current_file.path));
-                            // Find the Scaffold in the widget tree and use it to show a SnackBar.
-                            Scaffold.of(context).showSnackBar(snackBar);
-                            */
                             print("DELETING ${current_file}");
-
                             try {
                               current_file.deleteSync();
-                            } catch(i) {
-                              current_file = null;
-                            } 
-                            finally {
+                            } finally {
                               setState(() {});
                             }
                           },
@@ -114,11 +105,11 @@ class _GalleryPageState extends State<GalleryPage> {
                             File img = File(current_file.path);
                             Uint8List bytes = img.readAsBytesSync();
                             WcFlutterShare.share(
-                                        sharePopupTitle: 'share',
-                                        fileName: 'share.png',
-                                        mimeType: 'image/png',
-                                        bytesOfFile: bytes)
-                                    .then((value) => null);
+                                    sharePopupTitle: 'share',
+                                    fileName: 'share.png',
+                                    mimeType: 'image/png',
+                                    bytesOfFile: bytes)
+                                .then((value) => null);
                           },
                         ),
                       )),
@@ -136,17 +127,6 @@ class _GalleryPageState extends State<GalleryPage> {
                             },
                           )))
                 ])
-              /*
-              new GridView.builder(
-                  itemCount: photos.data.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 4.0,
-                      mainAxisSpacing: 4.0),
-                  itemBuilder: (BuildContext context, int index) {
-                    return Image.file(photos.data[index]);
-                  })
-                  */
               : new Center(
                   child: new Column(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -169,30 +149,6 @@ class _GalleryPageState extends State<GalleryPage> {
                     ]));
         },
       ),
-
-      /*
-      PhotoViewGallery.builder(
-        itemCount: imageList.length,
-        builder: (context, index) {
-          return PhotoViewGalleryPageOptions(
-            imageProvider: NetworkImage(
-              imageList[index],
-            ),
-            // Contained = the smallest possible size to fit one dimension of the screen
-            minScale: PhotoViewComputedScale.contained * 0.8,
-            // Covered = the smallest possible size to fit the whole screen
-            maxScale: PhotoViewComputedScale.covered * 2,
-          );
-        },
-        scrollPhysics: BouncingScrollPhysics(),
-        // Set the background color to the "classic white"
-        backgroundDecoration: BoxDecoration(
-          color: Theme.of(context).canvasColor,
-        ),
-        loadingChild: Center(
-          child: CircularProgressIndicator(),
-        ),
-        */
     );
   }
 }
